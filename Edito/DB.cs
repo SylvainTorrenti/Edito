22a6914b9b42979e10b6065eff42719abd06b21c
SylvainTorrenti <58286990+SylvainTorrenti@users.noreply.github.com>
2023-09-11 10:34:15 +0200

    Add comment DB.cs

--- Edito/DB.cs
+++ Edito/DB.cs
@@ -20,60 +20,97 @@ namespace Edito
         } 
         #endregion
         #region Articles
-
+        /// <summary>
+        /// Récupére les articles
+        /// </summary>
+        /// <returns>IEnuerable d'Article</returns>
         public IEnumerable<Article> GetArticles()
         {
             try
             {
                 _dbConnection.Open();
+                //Recuperation de tout les champs de la table article
                 var q = "SELECT * from article";
                 return _dbConnection.Query<Article>(q);
             }
             finally { _dbConnection.Close(); }
         }
-
+        /// <summary>
+        /// Ajoute un article
+        /// </summary>
+        /// <param name="titre">Le titre de l'article</param>
+        /// <param name="corps">Ce que l'article contient</param>
+        /// <param name="auteur">L'auteur de l'article peut être null</param>
+        /// <returns>L'id de l'article créé</returns>
         public int InsertArticle(string titre, string corps, string auteur)
         {
             try
             {
                 _dbConnection.Open();
+                //Insertion dans la table article avec comme valeur ce que nous avons fournit en paramétre
+                //Ajout d'une 2eme requête à la suite nous permettant de recuperer le dernier ID créé "SELECT LAST_INSERT_ID()"
                 var q = "INSERT INTO article (Titre,Corps,Auteur) VALUES (@titre, @corps, @auteur); SELECT LAST_INSERT_ID()";
+                //Execution de la requête grâce à "q" et Passage des valeurs voulues grâce à "new { titre, corps, auteur }"
                 var result = _dbConnection.Query<int>(q, new { titre, corps, auteur });
+                //Return result.Single() car même si un seul ID est récuperer il le donne sous forme de liste et nous devons en récupérer qu'un seul pour pouvoir l'utiliser.
                 return result.Single();
             }
             finally { _dbConnection.Close(); }
         }
-
+        /// <summary>
+        /// Mise a jour d'un article
+        /// </summary>
+        /// <param name="id">L'id de l'article à modifier</param>
+        /// <param name="titre">Le titre aprés modification</param>
+        /// <param name="corps">Le contenu aprés modification</param>
+        /// <param name="auteur">L'auteur aprés modification</param>
+        /// <returns></returns>
         public int UpdateArticle(int id, string titre, string corps, string auteur)
         {
             try
             {
                 _dbConnection.Open();
-                var q = "UPDATE Article SET Titre = @titre, Corps = @corps, Auteur = @auteur WHERE IDArticle = @id";
+                //Modification de l'article selectionné avec comme valeur ce que nous avons fournit en paramétre
+                var q = "UPDATE article SET Titre = @titre, Corps = @corps, Auteur = @auteur WHERE IDArticle = @id";
+                //Execution de la requête grâce à "q" et Passage des valeurs voulues grâce à "new { titre, corps, auteur }"
                 var result = _dbConnection.Execute(q, new { id, titre, corps, auteur });
                 return result;
             }
             finally { _dbConnection.Close(); }
         }
-
+        /// <summary>
+        /// Suppression d'un article
+        /// </summary>
+        /// <param name="id">L'id de l'article à supprimer</param>
+        /// <returns></returns>
         public int DeleteArticle(int id)
         {
             try
             {
                 _dbConnection.Open();
+                //Suprression de l'article selectionné 
                 var q = "DELETE from article WHERE IDArticle = @id";
+                //Execution de la requête grâce à "q" et Passage des valeurs voulues grâce à "new { id }"
                 var result = _dbConnection.Execute(q, new { id });
                 return result;
             }
             finally { _dbConnection.Close(); }
         }
+        /// <summary>
+        /// Verification si l'article est contenu dans un journal pour limiter la suppression
+        /// </summary>
+        /// <param name="id">L'id de l'article</param>
+        /// <returns>Le nombre de COUNT (d'occurence du champ selectionné) dans la table composition</returns>
         public long ArticleIsPresent(int id)
         {
             try
             {
                 _dbConnection.Open();
+                //Compte le nombre d'occurence du champs "IDArticle" pour verifier si l'article selectionné est lié à un/des journal/journaux
                 var q = "SELECT COUNT(c.IDArticle) from composition c join article a ON c.IDArticle = a.IDArticle WHERE a.IDArticle = @id;";
+                //Execution de la requête grâce à "q" et Passage des valeurs voulues grâce à "new { id }"
                 var result = _dbConnection.ExecuteScalar(q, new { id });
+                //Cast result en Long pour correspondre au retour nescessaire
                 return (long)result;
             }
             finally { _dbConnection.Close(); }
@@ -81,33 +118,57 @@ namespace Edito
 
         #endregion Articles
         #region NewsPaper
+        /// <summary>
+        /// Recuperation des journaux
+        /// </summary>
+        /// <returns>IEnumerable de NewsPaper</returns>
         public IEnumerable<NewsPaper> GetNewsPapers()
         {
             try
             {
                 _dbConnection.Open();
+                //Recuperation de tout les champs de la table article
                 var q = "SELECT * from journal";
                 return _dbConnection.Query<NewsPaper>(q);
             }
             finally { _dbConnection.Close(); }
         }
+        /// <summary>
+        /// ajoute un journal
+        /// </summary>
+        /// <param name="Titre">Le titre du journal</param>
+        /// <param name="DtParution">La date de parution du journal peut être null</param>
+        /// <returns></returns>
         public int InsertNewsPaper(string Titre, DateTime? DtParution)
         {
             try
             {
                 _dbConnection.Open();
+                //Insertion dans la table article avec comme valeur ce que nous avons fournit en paramétre
+                //Ajout d'une 2eme requête à la suite nous permettant de recuperer le dernier ID créé "SELECT LAST_INSERT_ID()"
                 var q = "INSERT INTO journal (Titre, DtParution) VALUES (@Titre, @DtParution); SELECT LAST_INSERT_ID() ";
+                //Execution de la requête grâce à "q" et Passage des valeurs voulues grâce à "new { titre, corps, auteur }"
                 var result = _dbConnection.Query<int>(q, new { Titre, DtParution });
+                //Return result.Single() car même si un seul ID est récuperer il le donne sous forme de liste et nous devons en récupérer qu'un seul pour pouvoir l'utiliser.
                 return result.Single();
             }
             finally { _dbConnection.Close(); }
         }
+        /// <summary>
+        /// Modification du journal
+        /// </summary>
+        /// <param name="IDNewsPaper">L'id du journal à modifier</param>
+        /// <param name="Titre">Titre du journal aprés modification</param>
+        /// <param name="DtParution">Date de parution du journal aprés modification</param>
+        /// <returns></returns>
         public int UpdateNewsPaper(int IDNewsPaper, string Titre, DateTime? DtParution)
         {
             try
             {
                 _dbConnection.Open();
+                //Modification du journal selectionné avec comme valeur ce que nous avons fournit en paramétre
                 var sql = "UPDATE journal SET Titre = @Titre, DtParution = @DtParution  WHERE IDJournal = @IDNewsPaper ;";
+                //Execution de la requête grâce à "q" et Passage des valeurs voulues grâce à "new { titre, corps, auteur }"
                 return _dbConnection.Execute(sql, new { IDNewsPaper, Titre, DtParution });
 
             }
@@ -116,11 +177,17 @@ namespace Edito
                 _dbConnection.Close();
             }
         }
+        /// <summary>
+        /// Suppression d'un journal
+        /// </summary>
+        /// <param name="IDNewsPaper">Id du journal à supprimer</param>
+        /// <returns></returns>
         public int DeleteNewsPaper(int IDNewsPaper)
         {
             try
             {
                 _dbConnection.Open();
+                //Suprression du journal selectionné 
                 var sql = "DELETE FROM journal WHERE IDJournal = @IDNewsPaper;";
                 return _dbConnection.Execute(sql, new { IDNewsPaper });
             }
@@ -129,13 +196,21 @@ namespace Edito
                 _dbConnection.Close();
             }
         }
+        /// <summary>
+        /// Verfication si le journal à supprimer contient un/des article(s)
+        /// </summary>
+        /// <param name="IDNewsPaper">Id du journal à verifier</param>
+        /// <returns></returns>
         public long NewsPaperIspresent(int IDNewsPaper)
         {
             try
             {
                 _dbConnection.Open();
+                //Compte le nombre d'occurence du champs "IDJournal" pour verifier si le journal selectionné est lié à un/des article(s)
                 var q = "SELECT COUNT(c.IDJournal) FROM composition c join journal j ON c.IDJournal = j.IDJournal WHERE j.IDJournal = @IDNewsPaper;";
+                //Execution de la requête grâce à "q" et Passage des valeurs voulues grâce à "new { IDNewsPaper }"
                 var result = _dbConnection.ExecuteScalar(q, new { IDNewsPaper });
+                //Cast result en Long pour correspondre au retour nescessaire
                 return (long)result;
             }
             finally
@@ -145,42 +220,67 @@ namespace Edito
         }
         #endregion
         #region Articles and NewsPaper
+        /// <summary>
+        /// Récupére les association entre les articles et les journaux
+        /// </summary>
+        /// <returns>IEnumerable d'Association</returns>
         public IEnumerable<Association> GetArticlesAsso()
         {
             try
             {
                 _dbConnection.Open();
+                //Recuperation de tout les champs de la table composition
                 var q = "SELECT * from composition;";
                 return _dbConnection.Query<Association>(q);
             }
             finally { _dbConnection.Close(); }
         }
+        /// <summary>
+        /// Récupére les articles present dans les journaux
+        /// </summary>
+        /// <param name="id">Id du journal selectionné</param>
+        /// <returns>IEnumerable d'Article</returns>
         public IEnumerable<Article> GetArticlesInNewspaper(int id)
         {
             try
             {
                 _dbConnection.Open();
+                // récupére les champs : "IDArticle" , "Titre" , "Corps" , "Auteur" de la table article qui sont lié au journal fournit par l'IDJournal donné
                 var q = "SELECT a.IDArticle , a.Titre , a.Corps , a.Auteur from article a join composition c on a.IDArticle = c.IDArticle join journal j on j.IDJournal = c.IDJournal WHERE c.IDJournal = @id";
                 return _dbConnection.Query<Article>(q, new { id });
             }
             finally { _dbConnection.Close(); }
         }
+        /// <summary>
+        /// Insertion de l'article dans la journal
+        /// </summary>
+        /// <param name="idNP">L'identifiant du journal selectionné</param>
+        /// <param name="idArticle">L'identifiant de l'article selectionné</param>
+        /// <returns>Un int pour verifier que la requête c'est effectué correctement</returns>
         public int InsertArticleInNewsPaper(int idNP, int idArticle)
         {
             try
             {
                 _dbConnection.Open();
+                //Insert dans la table composition la liaison entre l'article et le journal donné grace à idNP et idArticle
                 var q = "INSERT INTO composition (IDJournal, IDArticle) VALUES (@idNP, @idArticle);";
                 return _dbConnection.Execute(q, new { idNP, idArticle });
 
             }
             finally { _dbConnection.Close(); }
         }
+        /// <summary>
+        /// Suppression de l'association entre l'article et le journal dans la table composition
+        /// </summary>
+        /// <param name="idNP">L'identifiant du journal selectionné</param>
+        /// <param name="idArticle">L'identifiant de l'article selectionné</param>
+        /// <returns>Un int pour verifier que la requête c'est effectué correctement</returns>
         public int DelteArticleInNewsPaper(int idNP, int idArticle)
         {
             try
             {
                 _dbConnection.Open();
+                //Supprime la liaison entre l'article et le journal grâce au Id donné 
                 var q = "DELETE FROM composition WHERE IDJournal = @idNP AND IDArticle = @idArticle;";
                 return _dbConnection.Execute(q, new { idNP, idArticle });
 
